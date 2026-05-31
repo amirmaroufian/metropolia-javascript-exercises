8aebef90e08e0bfecda0467f06cdf2d4423842a7
Amir Maroufian <amirmaroufian@gmail.com>
2026-05-31 13:38:02 +0330

    add module-02 exercise-10

---
+++ module-02/exercise-06/script.js
@@ -0,0 +1,10 @@
+let numbers = [];
+
+for (let i = 0; i < 5; i++) {
+    let input = prompt(`Enter number ${i + 1}:`);
+    numbers.push(Number(input));
+}
+
+for (let i = numbers.length - 1; i >= 0; i--) {
+    console.log(numbers[i]);
+}
